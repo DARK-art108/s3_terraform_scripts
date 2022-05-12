851c796e6c53f23f12c66c3ee9105dfb1378503f
Ritesh Yadav <daydreamingguy941@gmail.com>
2022-05-12 22:26:45 +0530

    s3_bucket

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.region}"
+}
+
+module "s3" {
+    source = "<path-to-S3-folder>"
+    bucket_name = "<Bucket-name>"       
+}
